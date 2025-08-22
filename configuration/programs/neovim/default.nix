2fc34a44f39c91db0f20bef684d3d8432fe1fe84
Permafrozen <m8tt8o@gmail.com>
2025-08-22 21:35:06 +0200

    updated: nixvim, flake inputs

--- configuration/programs/neovim/default.nix
+++ configuration/programs/neovim/default.nix
@@ -7,5 +7,20 @@
 
   programs.nixvim = {
     enable = true;
+    clipboard.providers.wl-copy.enable = true;
+
+    plugins = {
+      lsp = {
+        enable = true;
+        servers = {
+          nixd = {
+            enable = true;
+            extraOptions = {
+              offset_encoding = "utf-8";
+            };
+          };
+        };
+      };
+    };
   };
 }
